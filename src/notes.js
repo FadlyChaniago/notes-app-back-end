91b17462f91c9af1789052e14f03c45b054f3b73
FadlyChaniago <fadlymuhamad313@gmail.com>
2023-11-03 11:37:41 +0700

    fix bugs host value

--- src/notes.js
+++ src/notes.js
@@ -1,12 +1,5 @@
 const notes = [
-  {
-    id: 'string', 
-    title: 'string',
-    createdAt: 'string', 
-    updatedAt: 'string',
-    tags: ['string'], 
-    body: 'string',
-  },
+ 
 ];
 
 module.exports = notes;
